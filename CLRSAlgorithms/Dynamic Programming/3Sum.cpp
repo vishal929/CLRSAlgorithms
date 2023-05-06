751dc34d008604748c1b50ce3238786900219735
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-06 14:26:41 -0400

    include 3 sum

---
+++ CLRSAlgorithms/Dynamic Programming/3Sum.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+/*
+    given a vector of integers, return tuples (nums[i],nums[j],nums[k]) where i \neq j \neq k such that the tuple sums to 0
+*/
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> solutions;
+    // sorting nums
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+        while (i > 0 && i < nums.size() && nums[i] == nums[i - 1]) i++;
+        int j = i + 1;
+        int k = nums.size() - 1;
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                //solution
+                vector<int> sol{ nums[i],nums[j],nums[k] };
+                solutions.push_back(sol);
+                j++;
+                // simple check to remove dupes
+                while (j < k && nums[j] == nums[j - 1]) j++;
+            }
+            else if (sum > 0) {
+                // overshoot
+                k--;
+            }
+            else {
+                // undershoot
+                j++;
+            }
+        }
+    }
+    return solutions;
+}

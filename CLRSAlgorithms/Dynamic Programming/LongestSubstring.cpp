7b076428251043fc1c18a001bc8c9823e2e83d8a
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-09 11:39:39 -0400

    longest substring linear time implementation

--- CLRSAlgorithms/Dynamic Programming/LongestSubstring.cpp
+++ CLRSAlgorithms/Dynamic Programming/LongestSubstring.cpp
@@ -2,34 +2,29 @@
 #include <set>
 
 /*
-    We find the length of the longest substring of s (substring is a contiguous portion of a string, NOT a subsequence)
+    We find the length of the longest substring of s without repeating characters
+    (substring is a contiguous portion of a string, NOT a subsequence)
     s: input string
 */
 int lengthOfLongestSubstring(string s) {
-    // M(i,j+1) is M(i,j)+1 if s[j+1] is not in M(i,j), and 0 otherwise 
+    // O(n) solution using two pointers
+    int best = 0;
+    int left = 0;
+    int curr = 0;
+    // only dealing with chars which are 1 byte (can just use a map otherwise)
+    vector<int> seen(256, -1);
 
-    int longest = 0;
-    set<char> seen;
     for (int i = 0; i < s.size(); i++) {
-        // longest so far is just 1 (s[i])
-        seen.insert(s[i]);
-        longest = max(longest,1)
-        for (int j = i + 1; j < s.size(); j++) {
-            if (seen.find(s[j]) == seen.end()) {
-                seen.insert(s[j]);
-                if (longest < seen.size()) {
-                    longest = seen.size();
-                }
-            }
-            else {
-                break;
+        if (seen[s[i]] != -1) {
+            // we have seen this before
+            for (; left < i; left++) {
+                seen[s[left]] = -1;
+                curr--;
             }
         }
-
-        if (longest < seen.size()) {
-            longest = seen.size();
-        }
-        seen.clear();
+        seen[s[i]] = i;
+        curr++;
+        best = max(best, curr);
     }
-    return longest;
+    return best;
 }

5062959cc170ed11a2bc37773d262d16fead936f
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-21 16:43:10 -0400

    add longest monotonically increasing subsequence

--- CLRSAlgorithms/Dynamic Programming/LongestCommonSubsequence.cpp
+++ CLRSAlgorithms/Dynamic Programming/LongestCommonSubsequence.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <algorithm>
 
 /*
 	Given 2 strings, return the string which represents the longest common subsequence
@@ -75,6 +76,67 @@ std::string LCS(std::string &s1, std::string &s2) {
 
 	// stack is the string in reversed order, so we just reverse and return!
 	string sol;
+	while (reverse_sol.size() > 0) {
+		sol.push_back(reverse_sol.top());
+		reverse_sol.pop();
+	}
+
+	return sol;
+}
+
+/*
+	Finds the longest monotonically increasing subsequence
+	nums is an array of integers
+*/
+std::vector<int> LMIS(std::vector<int> &nums) {
+	using namespace std;
+	// sort a copy of the array in O(nlgn)
+	vector<int> cop(nums);
+	sort(cop.begin(), cop.end());
+
+	// key idea: the longest monotonically increasing subsequence must be present in the sorted list!
+	// so, we just use LCS and reconstruct the solution
+	vector<vector<int>> M(nums.size()+1, vector<int>(nums.size()+1, 0));
+	
+	// substructure update
+	for (int i = 1; i < nums.size()+1; i++) {
+		for (int j = 1; j < nums.size()+1; j++) {
+			if (nums[i - 1] == cop[j - 1]) {
+				M[i][j] = 1 + M[i - 1][j - 1];
+			}
+			else {
+				M[i][j] = max(M[i - 1][j], M[i][j - 1]);
+			}
+		}
+	}
+
+	// recovering the solution
+	stack<int> reverse_sol;
+	stack<pair<int, int>> s;
+	s.push(make_pair(nums.size(), nums.size()));
+	while (s.size() > 0) {
+		pair<int, int> pos = s.top();
+		s.pop();
+
+		int i = pos.first, int j = pos.second;
+		if (nums[i - 1] == cop[j - 1]) {
+			// recurse at i-1,j-1 and update solution
+			s.push(make_pair(i - 1, j - 1));
+			reverse_sol.push(nums[i - 1]);
+		}
+		else {
+			// recurse at the better pathway
+			if (M[i - 1][j] > M[i][j - 1]) {
+				s.push(make_pair(i - 1, j));
+			}
+			else {
+				s.push(make_pair(i, j - 1));
+			}
+		}
+	}
+
+	// stack is the string in reversed order, so we just reverse and return!
+	vector<int> sol;
 	while (reverse_sol.size() > 0) {
 		sol.push_back(reverse_sol.top());
 		reverse_sol.pop();
